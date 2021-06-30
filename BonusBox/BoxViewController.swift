89ef93f7ca953a7c4bdb06e1b59509138f44c70f
KentaMaeda0916 <67748136+KentaMaeda0916@users.noreply.github.com>
2021-06-30 15:39:25 +0900

    ボタンとラベルを仮で配置

--- BonusBox/BoxViewController.swift
+++ BonusBox/BoxViewController.swift
@@ -8,13 +8,56 @@
 import UIKit
 
 class BoxViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        let buttonStackView = UIStackView(arrangedSubviews: [bonusBoxButton,penaltyButton])
+        buttonStackView.axis = .horizontal
+        
+        view.addSubview(operationGuidanceLable)
+        view.addSubview(buttonStackView)
+        
+        NSLayoutConstraint.activate([
+            operationGuidanceLable.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            operationGuidanceLable.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0)
+        ])
+        operationGuidanceLable.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            buttonStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
+            buttonStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0)
+        ])
+        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
+        
     }
-  
     
+    let operationGuidanceLable: UILabel = {
+        let label = UILabel()
+        label.text = "箱を選択してください"
+        return label
+    }()
+    
+    let bonusBoxButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .blue
+        button.imageView?.contentMode = .scaleAspectFit
+
+        return button
+    }()
+    
+    let penaltyButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .green
+        button.imageView?.contentMode = .scaleAspectFit
+
+        return button
+    }()
+    
+    private func setupLayout() {
+        
+    }
 
 }
--- BonusBox/UIViewExtention.swift
+++ BonusBox/UIViewExtention.swift
@@ -9,7 +9,7 @@ import UIKit
 
 extension UIView {
     
-    func achor(
+    func anchor(
         top: NSLayoutYAxisAnchor? = nil,
         bottom: NSLayoutYAxisAnchor? = nil,
         right: NSLayoutXAxisAnchor? = nil,
