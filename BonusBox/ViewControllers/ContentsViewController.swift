b83d016cfe22ee2972fb54a4a97dfd65c7a93f9b
KentaMaeda0916 <67748136+KentaMaeda0916@users.noreply.github.com>
2021-07-04 11:55:58 +0900

    contentTableViewの作成

--- BonusBox/ViewControllers/ContentsViewController.swift
+++ BonusBox/ViewControllers/ContentsViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ContentsViewController: UIViewController {
 
+    @IBOutlet weak var contentsTableVIew: UITableView!
+    
+    var contents = ["A","B","C"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        contentsTableVIew.dataSource = self
+        contentsTableVIew.delegate = self
+        
     }
 
 //    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -25,32 +32,45 @@ class ContentsViewController: UIViewController {
 //    }
 }
 
-extension ContentsViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width * 0.8, height: 50)
+extension ContentsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contents.count
     }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = contentsTableVIew.dequeueReusableCell(withIdentifier: "contentsCell", for: indexPath)
+        cell.textLabel?.text = contents[indexPath.row]
+        return cell
     }
+    
+    
 }
+//extension ContentsViewController: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: view.frame.width * 0.8, height: 50)
+//    }
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 10
+//    }
+//}
 
-class ContentsCell: UICollectionViewCell {
-    
-    let contentsTitle: UILabel = {
-        let label = UILabel()
-        label.text = "content1"
-        label.tintColor = .black
-        return label
-    }()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        backgroundColor = .white
-    }
-
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
+//class ContentsCell: UICollectionViewCell {
+//
+//    let contentsTitle: UILabel = {
+//        let label = UILabel()
+//        label.text = "content1"
+//        label.tintColor = .black
+//        return label
+//    }()
+//
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//
+//        backgroundColor = .white
+//    }
+//
+//
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//}
