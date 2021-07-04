1ab68079c43bc94b3e6e31a12b45f6e15d9b9b1c
KentaMaeda0916 <67748136+KentaMaeda0916@users.noreply.github.com>
2021-07-04 14:25:49 +0900

    ContentsVIewに編集ボタン追加

--- BonusBox/ViewControllers/ContentsViewController.swift
+++ BonusBox/ViewControllers/ContentsViewController.swift
@@ -19,17 +19,15 @@ class ContentsViewController: UIViewController {
         contentsTableVIew.dataSource = self
         contentsTableVIew.delegate = self
         
+        contentsTableVIew.allowsMultipleSelectionDuringEditing = true
+        navigationItem.title = "内容"
+        navigationItem.rightBarButtonItem = editButtonItem
+        
     }
-
-//    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return 10
-//    }
-//
-//    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
-//
-//        return cell
-//    }
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        contentsTableVIew.isEditing = editing
+    }
 }
 
 extension ContentsViewController: UITableViewDataSource, UITableViewDelegate {
@@ -42,35 +40,5 @@ extension ContentsViewController: UITableViewDataSource, UITableViewDelegate {
         cell.textLabel?.text = contents[indexPath.row]
         return cell
     }
-    
-    
-}
-//extension ContentsViewController: UICollectionViewDelegateFlowLayout {
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        return CGSize(width: view.frame.width * 0.8, height: 50)
-//    }
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-//        return 10
-//    }
-//}
 
-//class ContentsCell: UICollectionViewCell {
-//
-//    let contentsTitle: UILabel = {
-//        let label = UILabel()
-//        label.text = "content1"
-//        label.tintColor = .black
-//        return label
-//    }()
-//
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//
-//        backgroundColor = .white
-//    }
-//
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//}
+}
