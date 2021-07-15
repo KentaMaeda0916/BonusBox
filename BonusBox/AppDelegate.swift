8ecc7cce216adee96cfecd75d334059845c66c2a
KentaMaeda0916 <67748136+KentaMaeda0916@users.noreply.github.com>
2021-07-15 19:12:33 +0900

    初回起動時にdemoデータを追加

--- BonusBox/AppDelegate.swift
+++ BonusBox/AppDelegate.swift
@@ -14,6 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let userDefaults = UserDefaults.standard
+        let firstLunchKey = "firstLunchKey"
+        let firstLunch = [firstLunchKey: true]
+        userDefaults.register(defaults: firstLunch)
         return true
     }
 
--- BonusBox/ViewControllers/HomeTabViewController.swift
+++ BonusBox/ViewControllers/HomeTabViewController.swift
@@ -13,5 +13,16 @@ class HomeTabViewController: UITabBarController {
         super.viewDidLoad()
         
     }
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let userDefaults = UserDefaults.standard
+        let firstLunchKey = "firstLunchKey"
+        let demoBonusContents: [String] = ["アニメ１本見る","デザートを食べる","夜お酒を飲む"]
+        let demoPenaltyContents: [String] = ["スクワット１０回"]
+        
+        if userDefaults.bool(forKey: firstLunchKey) {
+            userDefaults.setValue(demoBonusContents, forKey: "bonus")
+            userDefaults.setValue(demoPenaltyContents, forKey: "penalty")
+        }
+    }
 }
