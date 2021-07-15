7636bbfd20d4e2b94ad6bb6feb3fea17e7e46665
KentaMaeda0916 <67748136+KentaMaeda0916@users.noreply.github.com>
2021-07-15 18:05:54 +0900

    キーボードを閉じるためのextensionの追加

--- BonusBox/Extention.swift
+++ BonusBox/Extention.swift
@@ -15,3 +15,20 @@ public func setUpButton(button: UIButton) {
     button.layer.shadowColor = UIColor.systemGray4.cgColor
     button.layer.shadowOffset = CGSize(width: 3, height: 3)
 }
+
+/// キーボードを閉じるためのextension
+extension UIViewController: UITextFieldDelegate{
+    
+    // リターンした時キーボード閉じる
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        textField.endEditing(true)
+        return true
+    }
+    
+    /// 他の部分をタップした時キーボード閉じる
+    open override func touchesBegan(_: Set<UITouch>, with _: UIEvent?) {
+        view.resignFirstResponder()
+        view.endEditing(true)
+    }
+}
